7028f101a7ef08c6b6367dbdb26f8674384d3be3
Sam Cooksley <samqweqwe3@hotmail.co.uk>
2018-02-13 19:22:54 +0000

    Improved performance of circle drawing.
    The number of calls to the SDL render function was reduced to once per
    circle. This was achieved by using a vector to store all the circle
    points to draw.

--- ParticleCollision/SDL_Functions.cpp
+++ ParticleCollision/SDL_Functions.cpp
@@ -1,42 +1,44 @@
 #include "SDL_Functions.h"
 
+#include <vector>
+
 namespace SDL
 {
   void DrawCircle(Renderer& _renderer, int _x, int _y, int _radius)
   {
     int x = _radius - 1, y = 0;
-	int dx = 1, dy = 1;
+    int dx = 1, dy = 1;
     int err = dx - (_radius << 1);
-	SDL_Renderer* renderer = _renderer.Get();
 
-	SDL_Point points[8];
+    std::vector<SDL_Point> points;
 
     while (x >= y)
     {
-		points[0] = { _x + x, _y + y };
-		points[1] = { _x + y, _y + x };
-		points[2] = { _x - y, _y + x };
-		points[3] = { _x - x, _y + y };
-		points[4] = { _x - x, _y - y };
-		points[5] = { _x - y, _y - x };
-		points[6] = { _x + y, _y - x };
-		points[7] = { _x + x, _y - y };
+      points.push_back({ _x + x, _y + y });
+      points.push_back({ _x + y, _y + x });
+      points.push_back({ _x - y, _y + x });
+      points.push_back({ _x - x, _y + y });
+      points.push_back({ _x - x, _y - y });
+      points.push_back({ _x - y, _y - x });
+      points.push_back({ _x + y, _y - x });
+      points.push_back({ _x + x, _y - y });
 
-		SDL_RenderDrawPoints(renderer, points, 8);
-
-        if (err <= 0)
-        {
-			++y;
-			err += dy;
-			dy += 2;
-        }
-        if (err > 0)
-        {
-			--x;
-			dx += 2;
-			err += dx - (_radius << 1);
-        }
+      if (err <= 0)
+      {
+        ++y;
+        err += dy;
+        dy += 2;
+      }
+      if (err > 0)
+      {
+        --x;
+        dx += 2;
+        err += dx - (_radius << 1);
+      }
     }
+
+    SDL_RenderDrawPoints(_renderer.Get(), &points[0], points.size());
+
   }
 
   void DrawPoint(Renderer& _renderer, int _x, int _y)
